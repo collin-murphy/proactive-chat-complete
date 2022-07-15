15b5fc4127788b8ec214c2bbdb1e11c62d70d773
Collin Murphy <55554147+collin-murphy@users.noreply.github.com>
2022-07-15 14:43:32 -0600

    Add files via upload

---
+++ custom button/proactive_chat.js
@@ -0,0 +1,19 @@
+//https://docs.microsoft.com/en-us/dynamics365/customer-service/start-proactive-chat
+
+// Wait for Chat widget to load completely
+window.addEventListener("lcw:ready", function handleLivechatReadyEvent() {
+    var timeToWaitBeforeOfferingProactiveChatInMilliseconds = 0000;//time to wait before Offering proactive chat to webpage visitor
+    // Setting context variables
+    Microsoft.Omnichannel.LiveChatWidget.SDK.setContextProvider(function contextProvider() {
+        return {
+            'Proactive Chat': { 'value': 'True', 'isDisplayable': true },
+            'Time On Page': { 'value': timeToWaitBeforeOfferingProactiveChatInMilliseconds, 'isDisplayable': true },
+            'Page URL': { 'value': window.location.href, 'isDisplayable': true },
+        };
+    });
+
+    //Display proactive chat invite after 'timeToWaitBeforeOfferingProactiveChatInMilliseconds' milliseconds
+    setTimeout(function () {
+        Microsoft.Omnichannel.LiveChatWidget.SDK.startProactiveChat({ message: "This is the proactive chat example text." }, false)
+    }, timeToWaitBeforeOfferingProactiveChatInMilliseconds);
+});
